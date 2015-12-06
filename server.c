ad8519f21420c50fc1e9269d0bb8da230c668f57
Chris Kopsidas <chris.kopsidas@gmail.com>
2015-12-07 01:11:00 +0200

    identation changes

--- server.c
+++ server.c
@@ -75,45 +75,39 @@ int main (void){
 
   /* Create the socket and set it up to accept connections. */
   sock = initialize_socket(PORT);
-  if (listen (sock, 1) < 0)
-    {
+  if (listen (sock, 1) < 0){
       perror ("listen");
       exit (EXIT_FAILURE);
-    }
+  }
 
   /* Initialize the set of active sockets. */
   FD_ZERO (&active_fd_set);
   FD_SET (sock, &active_fd_set);
 
-  while (1)
-    {
+  while (1){
       /* Block until input arrives on one or more active sockets. */
       read_fd_set = active_fd_set;
-      if (select (FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0)
-        {
+      if (select (FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0){
           perror ("select");
           exit (EXIT_FAILURE);
-        }
+      }
 
       /* Service all the sockets with input pending. */
       for (i = 0; i < FD_SETSIZE; ++i){
-        if (FD_ISSET (i, &read_fd_set))
-          {
-            if (i == sock)
-              {
+        if (FD_ISSET (i, &read_fd_set)){
+            if (i == sock){
                 /* Connection request on original socket. */
                 int new_conn;
                 size = sizeof (clientname);
                 new_conn = accept (sock, (struct sockaddr *)&clientname, (socklen_t * __restrict__)&size);
                 printf("FD_SETSIZE is %d, new connection request, just accepted in %d\n", FD_SETSIZE, i);
-                if (new_conn < 0)
-                  {
+                if (new_conn < 0){
                     printf("accept failure\n");
                     perror ("accept");
                     exit (EXIT_FAILURE);
-                  }
+                }
                 FD_SET (new_conn, &active_fd_set);
-              }
+            }
             else {
                 /* Data arriving on an already-connected socket. */
                 printf("WILL HANDLE %d\n", i);
@@ -121,9 +115,9 @@ int main (void){
                 close (i);
                 FD_CLR (i, &active_fd_set);
             }
-          }
-       }
-    }
+        }
+      }
+  }
 }
 
 int initialize_socket (uint16_t port){
@@ -132,48 +126,42 @@ int initialize_socket (uint16_t port){
 
   /* Create the socket. */
   sock = socket (PF_INET, SOCK_STREAM, 0);
-  if (sock < 0)
-    {
+  if (sock < 0){
       perror ("socket");
       exit (EXIT_FAILURE);
-    }
+  }
 
   /* Give the socket a name. */
   name.sin_family = AF_INET;
   name.sin_port = htons (port);
   name.sin_addr.s_addr = htonl (INADDR_ANY);
-  if (bind (sock, (struct sockaddr *) &name, sizeof (name)) < 0)
-    {
+  if (bind (sock, (struct sockaddr *) &name, sizeof (name)) < 0){
       perror ("bind");
       exit (EXIT_FAILURE);
-    }
+  }
   return sock;
 }
 
 int handle_request(int filedes){
   char buffer[MAXMSG];
   int nbytes;
-
   nbytes = read (filedes, buffer, MAXMSG);
-  if (nbytes < 0)
-    {
+  if (nbytes < 0){
       /* Read error. */
       perror ("read");
       exit (EXIT_FAILURE);
-    }
-  else if (nbytes == 0)
+  }
+  else if (nbytes == 0){
     /* End-of-file. */
     return -1;
-  else
-    {
+  |
+  else {
       /* Data read. */
       int len = strlen(buffer);
       buffer[len-1] = 0;
-      printf("\n\nServer: got message of len %d:\n '%s'\n\n\n", strlen(buffer), buffer);
-//      if(strncmp(buffer, "GET / HTTP/1.1", 14) == 0){
-        serve_request(filedes);
-        printf("RQUEST SERVED MUST NOT WAIT ANYMORE\n");
-//      }
+//      printf("\n\nServer: got message of len %d:\n '%s'\n\n\n", strlen(buffer), buffer);
+      serve_request(filedes);
+      printf("RQUEST SERVED MUST NOT WAIT ANYMORE\n");
       return 0;
-    }
+  }
 }
